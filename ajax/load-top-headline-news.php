36ec7ecbf15ff4be5ce1c5584b6265a04871ef98
Yasser Almeida <yasilvalmeida@hotmail.com>
2020-10-02 22:52:17 +0000

    Create pagination for 10 itens per page

--- ajax/load-top-headline-news.php
+++ ajax/load-top-headline-news.php
@@ -1,12 +1,17 @@
 <?php
-    if (isset($_POST['country']) && isset($_POST['category'])) {
+    if (isset($_POST['paginationIndex']) && 
+    isset($_POST['paginationItemPerIndex']) && 
+    isset($_POST['country']) && 
+    isset($_POST['category'])) {
+        $paginationIndex = $_POST["paginationIndex"];
+        $paginationItemPerIndex = $_POST["paginationItemPerIndex"];
         $country = $_POST["country"];
         $category = $_POST["category"];
         $api_key = "c91b92a7592e470a9e37d12ed5b5bf8e";
         if ($category != "all")
-            $url = "https://newsapi.org/v2/top-headlines?country=".$country."&category=".$category."&apiKey=".$api_key;
+            $url = "https://newsapi.org/v2/top-headlines?page=".$paginationIndex."&pageSize=".$paginationItemPerIndex."&country=".$country."&category=".$category."&apiKey=".$api_key;
         else
-            $url = "https://newsapi.org/v2/top-headlines?country=".$country."&apiKey=".$api_key;
+            $url = "https://newsapi.org/v2/top-headlines?page=".$paginationIndex."&pageSize=".$paginationItemPerIndex."&country=".$country."&apiKey=".$api_key;
         $response = file_get_contents($url);
         echo $response;
     }
--- js/index.js
+++ js/index.js
@@ -23,16 +23,67 @@ $(() => {
 });
 checkCountry = (country) => {
   //ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za
-  if (country.toLowerCase() == "st") return "pt";
+  if (country.toLowerCase() == "st") return "in";
   else return country;
 };
 loadNews = () => {
   loadByCategory(last_category);
 };
 loadByCategory = (category) => {
+  paginationIndex = 1;
   $("#" + last_category).removeClass("active");
   $("#" + category).addClass("active");
   last_category = category;
+  computePageGroup(category);
+};
+computePageGroup = (category) => {
+  var formData = new FormData();
+  formData.append("category", category.toLowerCase());
+  formData.append("country", country.toLowerCase());
+  $.ajax({
+    type: "post",
+    url: "ajax/count-top-headline-news.php",
+    data: formData,
+    contentType: false,
+    processData: false,
+    cache: false,
+    success: function (result) {
+      try {
+        var data = JSON.parse(result),
+          total = data.articles.length;
+        paginationGroup = Math.ceil(total / paginationItemPerIndex);
+        paginationDraw();
+        loadByIndex(category);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+};
+paginationDraw = () => {
+  var htmlPagination =
+    '<nav style="margin-left: 50px; margin-top: 20px"><ul class="pagination"><li class="page-item"><a class="page-link" href="javascript:loadPrev()" aria-label="Previous"><span aria-hidden="true"><i class="fas fa-arrow-alt-circle-left"></i></span></a></li>';
+  for (var i = 1; i <= paginationGroup; i++) {
+    if (paginationIndex == i)
+      htmlPagination +=
+        '<li class="page-item active"><a class="page-link" href="javascript:loadAt(' +
+        i +
+        ')">' +
+        i +
+        "</a></li>";
+    else
+      htmlPagination +=
+        '<li class="page-item"><a class="page-link" href="javascript:loadAt(' +
+        i +
+        ')">' +
+        i +
+        "</a></li>";
+  }
+  htmlPagination +=
+    '<li class="page-item"><a class="page-link" href="javascript:loadNext()" aria-label="Next"><span aria-hidden="true"><i class="fas fa-arrow-circle-right"></i></span></a></li></ul></nav>';
+  $("#paginationContent").html(htmlPagination);
+};
+loadByIndex = (category) => {
   var formData = new FormData(),
     newsContent = $("#newsContent");
   formData.append("category", category.toLowerCase());
@@ -50,37 +101,13 @@ loadByCategory = (category) => {
     success: function (result) {
       try {
         var data = JSON.parse(result),
-          total = data.articles.length;
+          total = data.articles.length,
+          htmlContent = "";
         if (data.status === "ok") {
           if (total == 0) {
             newsContent.html("No news for " + region);
             alert("No news for " + region);
           }
-          paginationGroup = Math.ceil(total / paginationItemPerIndex);
-          console.log(total);
-          console.log(paginationGroup);
-          var htmlPagination =
-            '<nav style="margin-left: 50px; margin-top: 20px"><ul class="pagination"><li class="page-item"><a class="page-link" href="javascript:loadPrev()" aria-label="Previous"><span aria-hidden="true"><i class="fas fa-arrow-alt-circle-left"></i></span></a></li>';
-          for (var i = 1; i <= paginationGroup; i++) {
-            if (paginationIndex == i)
-              htmlPagination +=
-                '<li class="page-item active"><a class="page-link" href="javascript:loadAt(' +
-                i +
-                ')">' +
-                i +
-                "</a></li>";
-            else
-              htmlPagination +=
-                '<li class="page-item"><a class="page-link" href="javascript:loadAt(' +
-                i +
-                ')">' +
-                i +
-                "</a></li>";
-          }
-          htmlPagination +=
-            '<li class="page-item"><a class="page-link" href="javascript:loadNext()" aria-label="Next"><span aria-hidden="true"><i class="fas fa-arrow-circle-right"></i></span></a></li></ul></nav>';
-          var htmlContent = "";
-          $("#paginationContent").html(htmlPagination);
           $.each(data.articles, (i, article) => {
             var title = article.title.substring(0, 50) + " ...",
               publishedAt = convertDate(article.publishedAt.split("T")[0]),
@@ -114,20 +141,23 @@ loadByCategory = (category) => {
     },
   });
 };
-loadNext = () => {
+loadPrev = () => {
   if (paginationIndex > 1) {
     paginationIndex--;
-    loadByCategory(last_category);
+    paginationDraw();
+    loadByIndex(last_category);
   }
 };
 loadAt = (num) => {
   paginationIndex = num;
-  loadByCategory(last_category);
+  paginationDraw();
+  loadByIndex(last_category);
 };
-loadPrev = () => {
+loadNext = () => {
   if (paginationIndex < paginationGroup) {
     paginationIndex++;
-    loadByCategory(last_category);
+    paginationDraw();
+    loadByIndex(last_category);
   }
 };
 convertDate = (inputFormat) => {
