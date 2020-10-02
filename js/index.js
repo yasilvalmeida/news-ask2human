c6e2d7c5bbc3e80b28e79f7cf11a8684945c1906
Yasser Almeida <yasilvalmeida@hotmail.com>
2020-10-02 23:15:19 +0000

    change header background color, change the logo image and change the menu to contact us to go to footer

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ $(() => {
 });
 checkCountry = (country) => {
   //ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za
-  if (country.toLowerCase() == "st") return "in";
+  if (country.toLowerCase() == "st") return "pt";
   else return country;
 };
 loadNews = () => {
